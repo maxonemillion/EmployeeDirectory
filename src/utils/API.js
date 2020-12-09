e7d8c476b4cc206c69b98125dce555778e9eccd4
Maximilian Daniel <max.daniel@utah.edu>
2020-12-09 01:43:22 -0700

    Updates

--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-export default {
-    getEmployees: function () {
-        return axios.get("https://randomuser.me/api/?inc=name,email,dob,picture,phone&nat=us&results=50")
-    }
-};
